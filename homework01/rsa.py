adfd4e4205b031c69b090c6bb9327c0009437205
pchizhov <32107377+pchizhov@users.noreply.github.com>
2017-10-08 20:49:35 +0300

    Update rsa.py

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -1,3 +1,6 @@
+import random
+
+
 def is_prime(n):
     """
     >>> is_prime(2)
@@ -7,7 +10,8 @@ def is_prime(n):
     >>> is_prime(8)
     False
     """
-    for i in range(1, n):
+    divisor = 1
+    for i in range(2, n):
         if n % i == 0:
             divisor = i
     if divisor == 1:
@@ -15,6 +19,7 @@ def is_prime(n):
     else:
         return False
 
+    
 def gcd(a, b):
     """
     >>> gcd(12, 15)
@@ -27,6 +32,7 @@ def gcd(a, b):
         a = b
         b = c
     return b
+
 
 def multiplicative_inverse(e, phi):
     """
@@ -75,3 +81,38 @@ def generate_keypair(p, q):
     # Return public and private keypair
     # Public key is (e, n) and private key is (d, n)
     return ((e, n), (d, n))
+
+
+def encrypt(pk, plaintext):
+    # Unpack the key into it's components
+    key, n = pk
+    # Convert each letter in the plaintext to numbers based on
+    # the character using a^b mod m
+    cipher = [(ord(char) ** key) % n for char in plaintext]
+    # Return the array of bytes
+    return cipher
+
+
+def decrypt(pk, ciphertext):
+    # Unpack the key into its components
+    key, n = pk
+    # Generate the plaintext based on the ciphertext and key using a^b mod m
+    plain = [chr((char ** key) % n) for char in ciphertext]
+    # Return the array of bytes as a string
+    return ''.join(plain)
+
+
+if __name__ == '__main__':
+    print("RSA Encrypter/ Decrypter")
+    p = int(input("Enter a prime number (17, 19, 23, etc): "))
+    q = int(input("Enter another prime number (Not one you entered above): "))
+    print("Generating your public/private keypairs now . . .")
+    public, private = generate_keypair(p, q)
+    print("Your public key is ", public, " and your private key is ", private)
+    message = input("Enter a message to encrypt with your private key: ")
+    encrypted_msg = encrypt(private, message)
+    print("Your encrypted message is: ")
+    print(''.join(map(lambda x: str(x), encrypted_msg)))
+    print("Decrypting message with public key ", public, " . . .")
+    print("Your message is:")
+    print(decrypt(public, encrypted_msg))
