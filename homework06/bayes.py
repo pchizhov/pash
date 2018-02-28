de474f0d3ce06d5e989aa4c791827c8b2b7e3e31
pchizhov <32107377+pchizhov@users.noreply.github.com>
2018-02-28 15:21:51 +0300

    Update bayes.py

--- homework06/bayes.py
+++ homework06/bayes.py
@@ -18,27 +18,22 @@ class NaiveBayesClassifier:
 
     def __init__(self, alpha=1):
         self.alpha = alpha
-        self.table = [[], [], [], [], [], [], []]
-        self.p_good = 0
-        self.p_maybe = 0
-        self.p_never = 0
+        self.labels = []
+        self.table = []
+        self.p_labels = []
 
     def fit(self, x, y):
-        """ Fit Naive Bayes classifier according to x, y. """
-        good, maybe, never = 0, 0, 0
+        """ Fit Naive Bayes classifier according to X, y. """
+        self.labels = [i for i in set(y)]
+        self.labels.sort()
+        classes = len(self.labels)
+        labels_count = [0] * classes
         for i in range(len(y)):
-            if y[i] == 'good':
-                y[i] = 1
-                good += 1
-            elif y[i] == 'maybe':
-                y[i] = 2
-                maybe += 1
-            elif y[i] == 'never':
-                y[i] = 3
-                never += 1
-        self.p_good = good / (good + maybe + never)
-        self.p_maybe = maybe / (good + maybe + never)
-        self.p_never = never / (good + maybe + never)
+            y[i] = self.labels.index(y[i]) + 1
+            labels_count[y[i] - 1] += 1
+
+        self.table = [[] for _ in range(classes * 2 + 1)]
+        self.p_labels = [math.log(number / sum(labels_count)) for number in labels_count]
 
         for i in range(len(x)):
             words = divide(x[i])
@@ -48,46 +43,36 @@ class NaiveBayesClassifier:
                 else:
                     self.table[0].append(word)
                     self.table[y[i]].append(1)
-                    self.table[y[i] % 3 + 1].append(0)
-                    self.table[(y[i] % 3 + 1) % 3 + 1].append(0)
-                    self.table[4].append(0)
-                    self.table[5].append(0)
-                    self.table[6].append(0)
+                    index = y[i]
+                    for j in range(classes - 1):
+                        index = (index % classes) + 1
+                        self.table[index].append(0)
+                    for column in range(classes + 1, classes * 2 + 1):
+                        self.table[column].append(0)
 
-        n_good = sum(self.table[1])
-        n_maybe = sum(self.table[2])
-        n_never = sum(self.table[3])
+        sums = [sum(self.table[i + 1]) for i in range(classes)]
         dim = len(self.table[0])
 
-        for i in range(len(self.table[0])):
-            self.table[4][i] = (self.table[1][i] + self.alpha) / (n_good + self.alpha * dim)
-            self.table[5][i] = (self.table[2][i] + self.alpha) / (n_maybe + self.alpha * dim)
-            self.table[6][i] = (self.table[3][i] + self.alpha) / (n_never + self.alpha * dim)
+        for line in range(dim):
+            for column in range(classes + 1, classes * 2 + 1):
+                self.table[column][line] = (self.table[column - classes][line] + self.alpha) / \
+                                           (sums[column - classes - 1] + self.alpha * dim)
 
     def predict(self, x):
         """ Perform classification on an array of test vectors X. """
         labels = []
+        classes = len(self.labels)
         for string in x:
-            string_good = math.log(self.p_good)
-            string_maybe = math.log(self.p_maybe)
-            string_never = math.log(self.p_never)
+            string_labels = [i for i in self.p_labels]
             words = divide(string)
             for word in words:
                 if word in self.table[0]:
-                    string_good += math.log(self.table[4][self.table[0].index(word)])
-                    string_maybe += math.log(self.table[5][self.table[0].index(word)])
-                    string_never += math.log(self.table[6][self.table[0].index(word)])
-                else:
-                    string_good += 0
-                    string_maybe += 0
-                    string_never += 0
-            probability = max(string_good, string_maybe, string_never)
-            if string_good == probability:
-                labels.append('good')
-            elif string_maybe == probability:
-                labels.append('maybe')
-            else:
-                labels.append('never')
+                    for i in range(classes):
+                        string_labels[i] += math.log(self.table[i + classes + 1][self.table[0].index(word)])
+            for i in range(classes):
+                if string_labels[i] == max(string_labels):
+                    labels.append(self.labels[i])
+                    break
         return labels
 
     def score(self, x_test, y_test):
